5ea2138dd455488d79220be7e80ae22382fc11ff
bohecola <bohecola@outlook.com>
2022-11-17 14:40:57 +0800

    add eslint

--- src/cool/service/request.js
+++ src/cool/service/request.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
+import axios from "axios";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import { ElMessage } from "element-plus";
 import { isDev, config } from "/@/cool";
 import { storage } from "/@/cool/utils";
 import { useBase } from "/$/base";
-import { router } from '../router';
+import { router } from "../router";
 
 NProgress.configure({ 
-  showSpinner: false 
+	showSpinner: false 
 });
 
 // 请求队列
@@ -18,132 +18,132 @@ let requests = [];
 let isRefreshing = false;
 
 const baseURL = import.meta.env.MODE === "development"
-  ? 'http://localhost:3000/api/admin'
-  : 'https://canday.site:3000/api/admin';
+	? "http://localhost:3000/api/admin"
+	: "https://canday.site:3000/api/admin";
 
 const service = axios.create({
-  baseURL: baseURL,
-  withCredentials: true,
-  timeout: 5000
+	baseURL: baseURL,
+	withCredentials: true,
+	timeout: 5000
 });
 
 service.interceptors.request.use(
-  (req) => {
-    const { user } = useBase();
+	(req) => {
+		const { user } = useBase();
 
-    if (req.url) {
-      // 请求进度条
-      if (!config.ignore.NProgress.some((e) => req.url.includes(e))) {
-        NProgress.start();
-      }
-    }
+		if (req.url) {
+			// 请求进度条
+			if (!config.ignore.NProgress.some((e) => req.url.includes(e))) {
+				NProgress.start();
+			}
+		}
 
-    // 请求信息
-    if (isDev) {
-      console.group(req.url);
-      console.log("method:", req.method);
-      console.table("data:", req.method == "get" ? req.params : req.data);
-      console.groupEnd();
-    }
+		// 请求信息
+		if (isDev) {
+			console.group(req.url);
+			console.log("method:", req.method);
+			console.table("data:", req.method == "get" ? req.params : req.data);
+			console.groupEnd();
+		}
 
-    // 验证token
-    if (user.token) {
-      // 请求标识
-      req.headers['Authorization'] = user.token;
+		// 验证token
+		if (user.token) {
+			// 请求标识
+			req.headers["Authorization"] = user.token;
 
-      if (req.url.includes("refreshToken")) {
-        return req;
-      }
+			if (req.url.includes("refreshToken")) {
+				return req;
+			}
 
-      // 判断 token 是否过期
-      if (storage.isExpired("token")) {
-        // 判断 refreshToken 是否过期
-        if (storage.isExpired("refreshToken")) {
-          return user.logout();
-        }
+			// 判断 token 是否过期
+			if (storage.isExpired("token")) {
+				// 判断 refreshToken 是否过期
+				if (storage.isExpired("refreshToken")) {
+					return user.logout();
+				}
 
-        // 是否在刷新中
-        if (!isRefreshing) {
-          isRefreshing = true;
+				// 是否在刷新中
+				if (!isRefreshing) {
+					isRefreshing = true;
 
-          user.refreshToken()
-            .then((token) => {
-              requests.forEach((cb) => cb(token));
-              requests = [];
-              isRefreshing = false;
-            })
-            .catch(() => {
-              user.clear();
-            });
-        }
+					user.refreshToken()
+						.then((token) => {
+							requests.forEach((cb) => cb(token));
+							requests = [];
+							isRefreshing = false;
+						})
+						.catch(() => {
+							user.clear();
+						});
+				}
 
-        return new Promise((resolve) => {
-          // 继续请求
-          requests.push((token) => {
-            // 重新设置 token
-            req.headers["Authorization"] = token;
-            resolve(req); // 等待refreshToken成功后才resolve前两次person和permmenu的req，让前两次请求继续执行
-          });
-        });
-      }
-    }
-    return req;
-  },
+				return new Promise((resolve) => {
+					// 继续请求
+					requests.push((token) => {
+						// 重新设置 token
+						req.headers["Authorization"] = token;
+						resolve(req); // 等待refreshToken成功后才resolve前两次person和permmenu的req，让前两次请求继续执行
+					});
+				});
+			}
+		}
+		return req;
+	},
 
-  error => {
-    return Promise.reject(error);
-  }
+	error => {
+		return Promise.reject(error);
+	}
 );
 
 service.interceptors.response.use(
-  (res) => {
-    NProgress.done();
+	(res) => {
+		NProgress.done();
 
-    if (!res?.data) {
-      return res;
-    }
+		if (!res?.data) {
+			return res;
+		}
 
-    const { code, data, message } = res.data;
+		const { code, data, message } = res.data;
 
-    if (!code) {
-      return res.data;
-    }
+		if (!code) {
+			return res.data;
+		}
 
-    switch (code) {
-      case 1000:
-        return data;
-      default:
-        return Promise.reject({ code, message });
-    }
-  },
+		switch (code) {
+		case 1000:
+			return data;
+		default:
+			return Promise.reject({ code, message });
+		}
+	},
 
-  async (error) => {
-    NProgress.done();
+	async (error) => {
+		NProgress.done();
 
-    if (error.response) {
-      const { status, config } = error.response;
-      const { user } = useBase();
+		if (error.response) {
+			const { status, config } = error.response;
+			const { user } = useBase();
       
-      if (status == 401) {
-        user.logout();
-      }
+			if (status == 401) {
+				user.logout();
+			}
 
-      if (isDev) {
-        ElMessage.error(`${config.url} ${status}`);
-      } else {
-        switch (status) {
-          case 403:
-            break;
-          case 500:
-            break;
-          case 502:
-            break;
-        }
-      }
-    }
+			if (isDev) {
+				ElMessage.error(`${config.url} ${status}`);
+			} else {
+				switch (status) {
+				case 403:
+					break;
+				case 500:
+					break;
+				case 502:
+					break;
+				}
+			}
+		}
 
-    return Promise.reject({ message: error.message });
-  }
+		return Promise.reject({ message: error.message });
+	}
 );
 
 export default service;
