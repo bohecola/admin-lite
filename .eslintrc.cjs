21c7bdd5b7e0b2dcc4f7880d90f988fc542c79f8
bohecola <bohecola@outlook.com>
2023-11-18 02:57:32 +0800

    chore: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,11 @@ module.exports = {
 	},
 	plugins: [
 		"vue"
-	]
+	],
+	rules: {
+		// Vue
+		"vue/multi-word-component-names": "off",
+		"vue/require-default-prop": "off",
+		"vue/require-prop-types": "off"
+	}
 };
