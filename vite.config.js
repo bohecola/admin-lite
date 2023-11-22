4dc9a9e337aa4a4733320c48797cd91e03f7b8de
bohecola <bohecola@outlook.com>
2023-11-22 14:56:31 +0800

    chore: add vite-plugin-vue-setup-extend

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,26 @@ import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueSetupExtend from "vite-plugin-vue-setup-extend";
+
 
 function resolve(dir) {
-  return path.resolve(__dirname, ".", dir);
+	return path.resolve(__dirname, ".", dir);
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [
-    vue(),
-    vueJsx()
-  ],
-  resolve: {
-    alias: {
-      "/@": resolve("src"),
-      "/$": resolve("src/modules")
-    },
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"]
-  }
-})
+	base: "/",
+	plugins: [
+		vue(),
+		vueJsx(),
+		vueSetupExtend()
+	],
+	resolve: {
+		alias: {
+			"/@": resolve("src"),
+			"/$": resolve("src/modules")
+		},
+		extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"]
+	}
+});
